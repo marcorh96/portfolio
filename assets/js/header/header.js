a8ce2cea443a7dfa939b6c3af252b8e408aa4c88
Marco Ramos <marco1584221@gmail.com>
2022-12-05 01:04:16 -0600

    agregado scrollspy

--- assets/js/header/header.js
+++ assets/js/header/header.js
@@ -7,9 +7,27 @@ navLinks.forEach( (l) => {
     {  if (menuToggle.classList.contains('show'))  bsCollapse.toggle(); });}); 
 
 
-function linkAction () {
+/*  function linkAction () {
   // Active Link
   navLink.forEach((n) => n.classList.remove('active'));
   this.classList.add('active');
 }
-navLink.forEach((n) => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener('click', linkAction));  */
+
+
+const navLi = document.querySelectorAll('nav div ul li a');
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        let sectionTop = section.offsetTop;
+        if (scrollY >= sectionTop - 65) {
+            current = section.getAttribute('id');
+        }
+    });
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        document.querySelector('nav div ul li a[href*= ' + current + ']').classList.add('active');
+    });
+});
